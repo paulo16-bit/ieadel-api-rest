d0ecc739ffca1523f49e6fa11a6626cea9eb1bda
Paulo Andre Correa Pereira <JT0009347@MATEUS.DC>
2025-09-26 11:51:23 -0300

    adição de HealthController

--- src/main/java/ex/infra/security/SecurityConfiguration.java
+++ src/main/java/ex/infra/security/SecurityConfiguration.java
@@ -29,8 +29,9 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/novo").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/congregacoes").permitAll()
                         .requestMatchers(HttpMethod.POST, "/movimentacoes").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/health").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/congregacoes").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
